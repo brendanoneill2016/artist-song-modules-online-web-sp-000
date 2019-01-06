793e5538321f68d2f150e68b6e098161f782b736
Brendan O'Neill <oneill.brendan88@gmail.com>
2019-01-06 13:44:23 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,17 +1,17 @@
 module Memorable 
   
-  
-  def reset_all
-  self.all.clear
-  end
-  
-  def count
-  self.all.count
-  end
-  
-  def initialize
-   self.all << self
-  end
-  
-  
-end 
+  module ClassMethods 
+    def reset_all
+    self.all.clear
+    end
+    
+    def count
+    self.all.count
+    end
+    
+    def initialize
+     self.all << self
+    end
+    
+    
+  end 
