f0535e7aea49c6f90b2edb71a3ca81f10c9fb6d1
Brendan O'Neill <oneill.brendan88@gmail.com>
2019-01-06 13:42:50 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -10,14 +10,10 @@ class Artist
 
   @@artists = []
 
-  # def initialize
-  #  @@artists << self
-  #  @songs = []
-  # end
-
-  # def self.find_by_name(name)
-  #  @@artists.detect{|a| a.name == name}
-  # end
+  def initialize
+     super
+     @songs = []
+  end
 
   def self.all
     @@artists
